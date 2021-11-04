5865149e47216185e8181a2c63b076be51e5e628
muchirijane <muchirijane@outlook.com>
2021-11-04 15:24:27 +0300

    added prettier formatter

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <h1>Sanity</h1>;
+  return <h1>Sanity</h1>
 }
--- sanity.js
+++ sanity.js
@@ -1,4 +1,4 @@
-import { createClient, createImageUrlBuilder } from "next-sanity";
+import { createClient, createImageUrlBuilder } from 'next-sanity'
 
 const config = {
   /**
@@ -8,20 +8,20 @@ const config = {
    *
    * https://nextjs.org/docs/basic-features/environment-variables
    **/
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: process.env.NODE_ENV === 'production',
   /**
    * Set useCdn to `false` if your application require the freshest possible
    * data always (potentially slightly slower and a bit more expensive).
    * Authenticated request (like preview) will always bypass the CDN
    **/
-};
+}
 /**
  * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
  * Read more: https://www.sanity.io/docs/image-url
  **/
-export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient = createClient(config);
+export const sanityClient = createClient(config)
